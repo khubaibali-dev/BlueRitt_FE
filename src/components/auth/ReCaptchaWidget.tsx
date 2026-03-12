a9549ee974f3c00bf6817166762e3909b4b4d4c3
khubaibalibhatti <alikhubaib959@gmail.com>
2026-03-12 16:23:06 +0500

    feat: Add ReCaptchaWidget component for Google reCAPTCHA integration.

--- src/components/auth/ReCaptchaWidget.tsx
+++ src/components/auth/ReCaptchaWidget.tsx
@@ -21,7 +21,7 @@ const ReCaptchaWidget: React.FC<ReCaptchaWidgetProps> = ({ onVerify }) => (
       */}
       <div className="scale-105 sm:scale-115 origin-center transition-transform duration-300">
         <ReCAPTCHA
-          sitekey="6LeTA4gsAAAAAA2d-sKC6meUlDFhcn2ITIyCIMKx"
+          sitekey="6LdhCIgsAAAAAFLtxV4wnIAfrCtwGjGtY1xwahB7"
           theme="dark"
 
           onChange={(token) => onVerify(!!token)}
